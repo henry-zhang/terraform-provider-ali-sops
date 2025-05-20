a31d23c0aaee1e98f7c407f8257284eb1463e564
Henry Zhang <henzhang@aligntech.com>
2025-05-20 15:44:33 +0800

    add demo code

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,32 @@
-module "ali_sops" {
-  source  = "app.terraform.io/maskway/ali-sops/provider"
-  version = "1.0.1"
+terraform {
+
+ required_providers {
+    sops = {
+      source = "henry-zhang/ali-sops"
+      version = "~> 1.0.3"
+    }
+
+}
+}
+
+data "sops_file" "demo-secret" {
+  source_file = "demo-secret.enc.json"
+}
+
+output "root-value-password" {
+  # Access the password variable from the map
+  value = data.sops_file.demo-secret.data["password"]
+  sensitive = true
+}
+
+output "mapped-nested-value" {
+  # Access the password variable that is under db via the terraform map of data
+  value = data.sops_file.demo-secret.data["db.password"]
+    sensitive = true
+}
+
+output "nested-json-value" {
+  # Access the password variable that is under db via the terraform object
+  value = jsondecode(data.sops_file.demo-secret.raw).db.password
+      sensitive = true
 }
